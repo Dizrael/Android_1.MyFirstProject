6cb0608ac77701e069391bc0a2e086894f1265b8
Admin <pashamyl999@gmail.com>
2020-06-13 00:22:58 +0600

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/android_1myfirstproject/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.android_1myfirstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
