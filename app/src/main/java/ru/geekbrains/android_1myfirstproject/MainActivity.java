3d0f28d7c3c81783626b8e0e8eaf0ee5565630f5
Admin <pashamyl999@gmail.com>
2020-06-16 17:06:10 +0600

    Commit 2. Refactoring

--- app/src/main/java/ru/geekbrains/android_1myfirstproject/MainActivity.java
+++ app/src/main/java/ru/geekbrains/android_1myfirstproject/MainActivity.java
@@ -1,14 +1,22 @@
 package ru.geekbrains.android_1myfirstproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import ru.geekbrains.android_1myfirstproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this));
+        setContentView(binding.getRoot());
+
     }
+
+
 }
